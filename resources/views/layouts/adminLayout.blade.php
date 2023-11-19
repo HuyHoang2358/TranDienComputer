0ae48ecbaf9192014afb423d47dbb044a94ad56a
Huyhoang-OH-28 <huyhoang.tran6669@gmail.com>
2023-11-19 15:13:35 +0700

    Integrate TiniMCE File-manager

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield("title") | Admin manager</title>
     @include("partials.adminHead")
+    @yield("head")
 </head>
 <body class="sidebar-main-menu">
     @include("partials.adminLoading")
